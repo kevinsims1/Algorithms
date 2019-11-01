4ae4db523c65ae3e75a56e4f0806e69400bef360
kevinsims1 <kevinsims1@outlook.com>
2019-11-01 10:27:17 -0500

    O(N) solution

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,15 +2,25 @@
 
 import argparse
 
+# def find_max_profit(prices):
+#   maximum = prices[1] - prices[0]
+#   for i in range(0, len(prices) - 1): 
+#     for k in range(i + 1, len(prices) - 1):
+#       if prices[k] - prices[i] > maximum:
+#         maximum = (prices[k] - prices[i])
+#         # print(prices[i], 'i')
+#         # print(prices[k], 'k')
+#         # print(maximum)
+#   return maximum
+
 def find_max_profit(prices):
-  maximum = prices[1] - prices[0]
-  for i in range(0, len(prices) - 1): 
-    for k in range(i + 1, len(prices) - 1):
-      if prices[k] - prices[i] > maximum:
-        maximum = (prices[k] - prices[i])
-        # print(prices[i], 'i')
-        # print(prices[k], 'k')
-        # print(maximum)
+  lowestBuy = prices[0]
+  maximum = prices[1] - lowestBuy
+  for i in range(1, len(prices) - 1):
+    if prices[i] - lowestBuy > maximum:
+      maximum = (prices[i] - lowestBuy)
+    if prices[i] < lowestBuy:
+      lowestBuy = prices[i]
   return maximum
 
 
